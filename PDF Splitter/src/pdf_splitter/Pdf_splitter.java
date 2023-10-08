d45e3403c0daad607df5dad0df9e75e63330e284
Sushil Patil <129952964+sushilvpatil@users.noreply.github.com>
2023-10-08 10:40:02 +0530

    second project
    i have completed pdf splitter project assigned by code clause. in this project i have used java language, this project splites the pdf into number of desired pages according to user need.

---
+++ PDF Splitter/src/pdf_splitter/Pdf_splitter.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pdf_splitter;
+
+/**
+ *
+ * @author HARSHAL
+ */
+public class Pdf_splitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        DashBoard db=new DashBoard();
+        db.setVisible(true);
+    }
+    
+}
